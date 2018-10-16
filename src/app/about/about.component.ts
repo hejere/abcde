53e181335ddad97c1af37d63da31bdaee3cc7def
hejere <lasmarhejer93@gmail.com>
2018-10-16 16:59:04 +0200

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'; 
+import { DataService } from '../data.service';
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+goals = [];
+  constructor(private router: Router , private _data: DataService) { }
+
+  ngOnInit() {
+    this._data.goal.subscribe(res=> this.goals=res)
+  }
+  sendMeHome() {
+    this.router.navigate(['']); //''pour la route Home meree
+    return false;
+  }
+}
